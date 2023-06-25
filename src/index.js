5f6f514d7dba726b62da47c9dd8118a2442908f3
Object_417 <belovedofthelord1@gmail.com>
2023-06-25 21:26:21 +0100

    fixes #1

--- src/index.js
+++ src/index.js
@@ -1,110 +1,114 @@
+import "./styles/index.css"
+
 // FIXME: Import SVG as a string and embed it as HTML...
 // FIXME: There must a better way
-import { xMarkSVG, oMarkSVG } from "./icons/icons.js";
+import { xMarkSVG, oMarkSVG } from "./icons/icons.js"
 
-const $board = document.getElementById("board");
-const $playerMove = document.getElementById("playerMove");
-const $btnRestart = document.getElementById("btnRestart");
+const $board = document.getElementById("board")
+const $playerMove = document.getElementById("playerMove")
+const $btnRestart = document.getElementById("btnRestart")
 
 // FIXME: Maybe, use indexes instead of ids
 // FIXME: Or at least, object instead of array
 const players = [
   { id: 0, val: "X", html: xMarkSVG },
-  { id: 1, val: "O", html: oMarkSVG },
-];
+  { id: 1, val: "O", html: oMarkSVG }
+]
 
 // Player with id = 0 goes first
-let playerMove = 0;
-let moveNo = 0;
+let playerMove = 0
+let moveNo = 0
 
 let fieldSize = prompt("Field size (x, y)", "3, 3")
   .split(",")
-  .map((el) => Number(el));
-const SCORE_FOR_VICTORY = Number(prompt("Number of marks for victory", 3));
+  .map((el) => Number(el))
+const SCORE_FOR_VICTORY = Number(prompt("Number of marks for victory", 3))
 
-console.log(fieldSize);
-console.log(SCORE_FOR_VICTORY);
+console.log(fieldSize)
+console.log(SCORE_FOR_VICTORY)
 
-let cells = createCells(...fieldSize);
+let cells = createCells(...fieldSize)
 function createCells(nOfRows, nOfCols) {
-  const nOfCells = nOfRows * nOfCols;
+  const nOfCells = nOfRows * nOfCols
 
-  $board.style.display = "grid";
-  $board.style.gridTemplate = `repeat(${nOfRows}, 5rem) / repeat(${nOfCols}, 5rem)`;
+  $board.style.display = "grid"
+  $board.style.gridTemplate = `repeat(${nOfRows}, 5rem) / repeat(${nOfCols}, 5rem)`
 
   return Array.from({ length: nOfCells }).map(($cell, index) => {
-    $cell = document.createElement("div");
-    $cell.classList.add("cell");
-    $cell.setAttribute("id", "cell" + index);
-    $board.append($cell);
+    $cell = document.createElement("div")
+    $cell.classList.add("cell")
+    $cell.setAttribute("id", "cell" + index)
+    $board.append($cell)
 
     return {
       id: index,
       elem: $cell,
       val: null,
       x: index - Math.floor(index / nOfCols) * nOfCols,
-      y: Math.floor(index / nOfRows),
-    };
-  });
+      y: Math.floor(index / nOfCols)
+    }
+  })
 }
 
+console.log(cells)
+
 function getCell(id) {
-  return cells.find((cell) => cell.id === id);
+  return cells.find((cell) => cell.id === id)
 }
 
 function getCellByCoordinates(x, y) {
-  return cells.find((cell) => cell.x === x && cell.y === y);
+  return cells.find((cell) => cell.x === x && cell.y === y)
 }
 
 function setCellValue(cell, player) {
   // Do nothing if this cell is already taken
   if (cell.val) {
-    return;
+    return
   }
 
-  cell.val = player.val;
-  cell.elem.innerHTML = player.html;
+  cell.val = player.val
+  cell.elem.innerHTML = player.html
 
-  moveNo++;
-  playerMove++;
+  moveNo++
+  playerMove++
   if (!getPlayer(playerMove)) {
-    playerMove = 0;
+    playerMove = 0
   }
-  updateTitle(getPlayer(playerMove));
+  updateTitle(getPlayer(playerMove))
   if (moveNo >= fieldSize[0] * fieldSize[1]) {
-    $playerMove.textContent = "Draw";
-    $btnRestart.style.visibility = "visible";
-    $btnRestart.toggleAttribute("disabled", false);
+    $playerMove.textContent = "Draw"
+    $btnRestart.style.visibility = "visible"
+    $btnRestart.toggleAttribute("disabled", false)
   }
 }
 
 function getPlayer(id) {
-  return players.find((player) => player.id === id);
+  return players.find((player) => player.id === id)
 }
 
 function updateTitle(player) {
   if (gameOver) {
-    $playerMove.textContent = `Player ${player.val} has won`;
+    $playerMove.textContent = `Player ${player.val} has won`
   } else {
-    $playerMove.textContent = `Player ${player.val} move`;
+    $playerMove.textContent = `Player ${player.val} move`
   }
 }
 
 function setGameOver(gameOver, player) {
   if (!gameOver) {
-    return;
+    return
   }
 
-  updateTitle(player);
-  $btnRestart.style.visibility = "visible";
-  $btnRestart.toggleAttribute("disabled", false);
+  updateTitle(player)
+  $btnRestart.style.visibility = "visible"
+  $btnRestart.toggleAttribute("disabled", false)
 }
 
 function testForVictory(checkedCells, cell, player, score, relation) {
-  let x = cell.x;
-  let y = cell.y;
+  let x = cell.x
+  let y = cell.y
 
-  [
+  ;[
     { x: x - 1, y: y, relation: "h" },
     { x: x + 1, y: y, relation: "h" },
     { x: x, y: y + 1, relation: "v" },
@@ -112,11 +116,9 @@ function testForVictory(checkedCells, cell, player, score, relation) {
     { x: x + 1, y: y + 1, relation: "ld" },
     { x: x - 1, y: y - 1, relation: "ld" },
     { x: x - 1, y: y + 1, relation: "rd" },
-    { x: x + 1, y: y - 1, relation: "rd" },
+    { x: x + 1, y: y - 1, relation: "rd" }
   ].forEach((el) => {
-    const nearbyCell = getCellByCoordinates(el.x, el.y);
-
-    // debugger
+    const nearbyCell = getCellByCoordinates(el.x, el.y)
 
     if (
       nearbyCell === undefined ||
@@ -124,13 +126,11 @@ function testForVictory(checkedCells, cell, player, score, relation) {
       checkedCells.includes(nearbyCell.id) ||
       (relation && el.relation !== relation)
     ) {
-      return;
+      return
     }
 
-    score[el.relation] += 1;
-    checkedCells.push(cell.id);
-
-    // debugger
+    score[el.relation] += 1
+    checkedCells.push(cell.id)
 
     if (
       score.h === SCORE_FOR_VICTORY ||
@@ -138,15 +138,15 @@ function testForVictory(checkedCells, cell, player, score, relation) {
       score.rd === SCORE_FOR_VICTORY ||
       score.ld === SCORE_FOR_VICTORY
     ) {
-      return;
+      return
     }
 
-    testForVictory(checkedCells, nearbyCell, player, score, el.relation);
-  });
+    testForVictory(checkedCells, nearbyCell, player, score, el.relation)
+  })
 }
 
-let gameOver = false;
-let playerWon = null;
+let gameOver = false
+let playerWon = null
 
 $board.onclick = (e) => {
   if (
@@ -154,37 +154,39 @@ $board.onclick = (e) => {
     e.target.closest(".cell") === null ||
     gameOver === true
   ) {
-    return;
+    return
   }
-  const $cell = e.target.closest(".cell");
-  const cellId = Number($cell.id.replace("cell", ""));
-  const cell = getCell(cellId);
+  const $cell = e.target.closest(".cell")
+  const cellId = Number($cell.id.replace("cell", ""))
+  const cell = getCell(cellId)
 
-  const player = getPlayer(playerMove);
-  setCellValue(cell, player);
+  const player = getPlayer(playerMove)
+  setCellValue(cell, player)
 
-  const score = { h: 1, v: 1, rd: 1, ld: 1 };
-  testForVictory([cell.id], cell, player, score);
+  const score = { h: 1, v: 1, rd: 1, ld: 1 }
+
+  testForVictory([cell.id], cell, player, score)
+
   if (
     score.h === SCORE_FOR_VICTORY ||
     score.v === SCORE_FOR_VICTORY ||
     score.rd === SCORE_FOR_VICTORY ||
     score.ld === SCORE_FOR_VICTORY
   ) {
-    gameOver = true;
-    playerWon = player;
-    setGameOver(gameOver, playerWon);
-    return;
+    gameOver = true
+    playerWon = player
+    setGameOver(gameOver, playerWon)
+    return
   }
-};
+}
 
 $btnRestart.onclick = () => {
-  $board.innerHTML = "";
-  cells = createCells(...fieldSize);
-  gameOver = false;
-  playerMove = 0;
-  moveNo = 0;
-  updateTitle(getPlayer(playerMove));
-  $btnRestart.style.visibility = "hidden";
-  $btnRestart.toggleAttribute("disabled", true);
-};
+  $board.innerHTML = ""
+  cells = createCells(...fieldSize)
+  gameOver = false
+  playerMove = 0
+  moveNo = 0
+  updateTitle(getPlayer(playerMove))
+  $btnRestart.style.visibility = "hidden"
+  $btnRestart.toggleAttribute("disabled", true)
+}
